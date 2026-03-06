cde47738768aacec770e62084a6121c21d15b1dc
manojtravelworld-ship-it <manoj.travelworld@gmail.com>
2026-03-06 14:09:34 +0530

    Update server.ts

--- server.ts
+++ server.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
-import  errorHandler  from './utils/errorHandler';
+import  errorHandler  from './utils/errorHandler.js';
 import { createServer as createViteServer } from 'vite';
 
 // Load environment variables
