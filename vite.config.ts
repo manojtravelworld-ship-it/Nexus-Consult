abab685ec87c3b4dfb1c3eb6fef05ec8bd19dfbc
manojtravelworld-ship-it <manoj.travelworld@gmail.com>
2026-03-06 14:30:25 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server:{
+    allowedHosts:
+      ['nexus-consult-production.up.railway.app']
+         }
 });
